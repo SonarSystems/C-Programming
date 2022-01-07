1fbd7e229f6e766fa78e20952e3e30128ed966f6
Sonar Systems <SonarSystems@users.noreply.github.com>
2022-01-07 16:12:07 +0000

    Source Code
    -Function Declaration
    -Variable Scope

---
+++ Code/[8] Functions/Function Declaration/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void Print( );
+
+int main( )
+{
+   Print( );
+   Print( );
+   Print( );
+   Print( );
+
+   return 0;
+}
+
+void Print( )
+{
+   printf( "Functions" );
+}
---
+++ Code/[8] Functions/Variable Scope/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void Adder( )
+{
+   int num1 = 10;
+   int num2 = 5;
+
+   printf( "%i", num1 + num2 );
+}
+
+int main( )
+{
+   Adder( );
+
+   for ( int i = 0; i < 10; i++ )
+   {
+      int j = 8;
+   }
+
+   j = 7;
+
+   return 0;
+}
