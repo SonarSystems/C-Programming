1db2cd5a7660e2ebc4576e186020bd37d1a15b45
Sonar Systems <SonarSystems@users.noreply.github.com>
2023-03-10 10:43:39 +0000

    Initialize Arrays Inline Code

---
+++ Code/[9] Arrays/Initialize Arrays Inline/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main( )
+{
+   int highscores[] = { 1000, 800, 700, 300, 298 };
+
+   printf( "#1 - %i\n", highscores[0] );
+   printf( "#2 - %i\n", highscores[1] );
+   printf( "#3 - %i\n", highscores[2] );
+   printf( "#4 - %i\n", highscores[3] );
+   printf( "#5 - %i\n", highscores[4] );
+
+   return 0;
+}
