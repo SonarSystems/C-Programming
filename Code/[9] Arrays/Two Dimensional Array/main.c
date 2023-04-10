32ea291649b6b0d16674e10529d67083c7da5d68
Sonar Systems <SonarSystems@users.noreply.github.com>
2023-04-10 08:33:12 +0100

    Two Dimensional Array Code

---
+++ Code/[9] Arrays/Two Dimensional Array/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main( )
+{
+   int values[3][4] = { { 4, 2, 6 }, { 7, 1 ,9 }, { 40, 23, 67 } };
+
+   /*values[0][0] = 4;
+   values[0][1] = 2;
+   values[0][2] = 6;
+
+   values[1][0] = 7;
+   values[1][1] = 1;
+   values[1][2] = 9;
+
+   values[2][0] = 40;
+   values[2][1] = 23;
+   values[2][2] = 67;*/
+
+   printf( "%i", values[0][0] );
+
+   return 0;
+}
